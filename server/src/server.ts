ba60bbd75f78612d273998eb4f926a000c7984fb
abdalrzag eisa <bmjfdrh@gmail.com>
2023-06-27 00:34:23 +0300

    add server

---
+++ server/src/server.ts
@@ -0,0 +1,16 @@
+import express, { Express, Request, Response } from 'express'
+
+const app: Express = express()
+const port = 3333
+
+import cors from 'cors'
+
+app.use(cors({ origin: 'http://localhost:3000' }))
+
+app.get('/api', (req: Request, res: Response) => {
+    res.json({ name: 'senpai' })
+})
+
+app.listen(port, () => {
+    console.log(`[Server]: I am running at https://localhost:${port}`)
+})
